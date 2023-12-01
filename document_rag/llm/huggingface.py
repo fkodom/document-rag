6f1dd18b959837681a4cc1b458bdeb0ecb1d87bd
Frank Odom <frank.odom.iii@gmail.com>
2023-12-01 13:36:47 -0600

    Small fixes to Huggingface LLM, and automatic device mapping

--- document_rag/llm/huggingface.py
+++ document_rag/llm/huggingface.py
@@ -3,6 +3,9 @@ from typing import Optional, TypedDict, cast
 from transformers import pipeline
 
 from document_rag.llm.base import BaseLLM
+from document_rag.settings import Settings
+
+SETTINGS = Settings()
 
 
 class Response(TypedDict):
@@ -15,11 +18,16 @@ class HuggingFaceLLM(BaseLLM):
     def __init__(
         self, model: str, do_sample: bool = False, token: Optional[str] = None
     ):
+        if not token:
+            token = SETTINGS.HUGGINGFACE_TOKEN
+
         self.model = model
         self.do_sample = do_sample
-        self.pipeline = pipeline("text-generation", model=self.model, token=token)
+        self.pipeline = pipeline(
+            "text-generation", model=self.model, device_map="auto", token=token
+        )
 
     def generate(self, prompt: str) -> str:
         """Generate text from a prompt using the OpenAI API."""
-        response = cast(Response, self.pipeline(prompt, do_sample=False)[0])
+        response = cast(Response, self.pipeline(prompt)[0])
         return response["generated_text"]
